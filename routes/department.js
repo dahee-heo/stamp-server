6522fe6aa888ee270e9de715fd7ef9f483fd62c8
dahee-heo <hdahee.9@gmail.com>
2022-07-10 13:36:19 +0900

    department post, update, delete

--- routes/department.js
+++ routes/department.js
@@ -4,16 +4,19 @@ const Department = require('../schema/department.schema');
 
 
 router.post('/', async function (req, res, next) {
+  // await newDepartment.save()
+  const departmentFind = await Department.findOne({ department: req.body.department })
+  if (departmentFind) {
+    res.json(departmentFind)
+    return
+  }
+
   const newDepartment = new Department({
     department: req.body.department,
   })
-  // await newDepartment.save()
 
-  Department.findOne({ department: req.body.department }, (err, result) => {
-    if (!result) {
-      newDepartment.save()
-    }
-  })
+  const departmentSave = await newDepartment.save()
+  res.json(departmentSave)
 })
 
 router.get('/', async function (req, res, next) {
@@ -23,6 +26,19 @@ router.get('/', async function (req, res, next) {
   const department = await Department.find().select('department')
   res.json(department)
 
+})
+
+router.put('/', async function (req, res, next) {
+  const departmentUpdate = await Department.findOneAndUpdate({ _id: req.body._id }, { department: req.body.department })
+  res.json(departmentUpdate)
+  console.log(departmentUpdate)
+
+
+})
+
+router.delete('/', async function (req, res, next) {
+  const departmentDelete = await Department.deleteOne({ _id: req.body._id })
+  res.json(departmentDelete)
 })
 
 router.get('/:id', async function (req, res, next) {
