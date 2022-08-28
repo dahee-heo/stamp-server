f81f0732910f375c590f74754e91e58bb1388f19
dahee-heo <hdahee.9@gmail.com>
2022-08-28 14:06:39 +0900

    mongoDB option

--- util/mongoose.util.js
+++ util/mongoose.util.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+const config = require('../secret/config')
 
 const mongooseInit = async () => {
-  return await mongoose.connect('mongodb://localhost:27017/stamp')
+  const mongoDBUrl = config[process.env.NODE_ENV]?.mongoDBOptions?.url ?? config?.LOCAL?.mongoDBOptions?.url
+  return await mongoose.connect(mongoDBUrl)
 }
 
 module.exports = {
